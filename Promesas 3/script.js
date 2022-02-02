8b9a35766d0077fee14d3f48a37f71b46207f08d
Your Name <you@example.com>
2022-02-02 21:36:17 +0100

    cambios en promesas

--- Promesas 3/script.js
+++ Promesas 3/script.js
@@ -4,7 +4,7 @@ let getBatman = new Promise((resolve, reject) => {
     ajax2 = new XMLHttpRequest();
     ajax2.open('GET', 'http://api.tvmaze.com/search/shows?q=batman');
     ajax2.addEventListener('load', () => {
-        console.log('Resolvermos Batman');
+        console.log('Recuperados datos de Batman, objeto promesa creado');
         resolve(ajax2.responseText);
     });
     ajax2.send();
@@ -15,7 +15,7 @@ let getSpiderman = new Promise((resolve, reject) => {
     ajax = new XMLHttpRequest();
     ajax.open('GET', 'http://api.tvmaze.com/search/shows?q=spiderman');
     ajax.addEventListener('load', () => {
-        console.log('Resolvermos Spiderman');
+        console.log('Recuperados datos de Spiderman, objeto promesa creado');
         resolve(getBatman); // Esto va a ser Batman en el parámetro de abajo
     });
     ajax.send();
@@ -23,12 +23,12 @@ let getSpiderman = new Promise((resolve, reject) => {
 
 getSpiderman
     .then((Batman) => {
-        console.log('Promesa Spiderman: ', ajax.responseText);
+        console.log('Resuelta Spiderman: ', ajax.responseText);
         return (Batman);
     })
     .then((datos) => {
         console.log('----------------------------------------------');
-        console.log('Promesa Batman: ', datos);
+        console.log('Resuelta Batman: ', datos);
     })
     .catch((error) => {
         console.log('Error: ', data);
